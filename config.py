0a9d64e3cb7726e6909601e1df00b319c67f32cb
oWlogona <oWlogona>
2020-12-17 18:56:36 +0200

    update

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+URL_BEATS = os.getenv("URL_BEATS")
+URL_WATCH = os.getenv("URL_WATCH")
+ADMIN_ID = 732241058
